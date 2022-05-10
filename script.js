b60de25f640846b51da640698c93e35552a0beb3
Tomas pujolle <alan_pujolle@hotmail.com>
2022-05-10 00:56:57 -0300

    entrega de desafio

---
+++ script.js
@@ -0,0 +1,36 @@
+let dolar = 198.5
+let euro =123.28
+let yen =0.90
+let libra =144.08 
+
+function convertidor(pesos, dolar, euro, yen, libra, moneda) {
+    switch (moneda) {
+        case "dolar":
+            return pesos / dolar;
+            break;
+        case "euro":
+            return pesos / euro;
+            break;
+        case "yen":
+            return pesos / yen;
+            break;
+        case "libra":
+            return pesos / libra;
+            break;
+        default:
+            return error;
+            break;
+    }
+}
+
+ 
+let salir = "1";
+while(salir == "1"){
+    let pesos=parseInt(prompt("Ingresa valor en pesos Argentinos"));
+    let moneda=prompt("Elegir entre: \n dolar \n euro \n yen \n libra");
+    let resultado=convertidor(pesos, dolar, euro, yen, libra, moneda);
+    console.log("Resultado de la conversion = $"+resultado);
+    alert("Total en la moneda seleccionada es: $"+resultado);
+    salir=prompt("1 - Para seguir convirtiendo \n 2 - para salir ");
+}
+
